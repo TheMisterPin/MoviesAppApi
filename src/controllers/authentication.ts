2ff6e701238e614fa594718a6fa742cf947f052e
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2023-11-03 08:52:53 +0100

    implemented login function

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -4,6 +4,30 @@ import { random } from '../helpers'
 import { authentication } from '../helpers/index';
 
 
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Missing required fields' })
+        }
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication')
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' })
+        }
+    const encryptedPassword = authentication(user.authentication.salt, password)
+        if (authentication(user.authentication.salt, password) !== encryptedPassword) {
+            return res.status(403).json({ message: 'Invalid password' })
+        }
+        const salt = random()
+        user.authentication.sessionToken = authentication(salt, user._id.toString() )
+        res.cookie('sessionToken', user.authentication.sessionToken, { domain : 'localhost', path : '/'})
+        return res.status(200).json({ message: 'Login successful' })
+    }
+    catch (error) {
+        return res.status(500).json({ message: 'Internal server error' })
+    }
+}
+
 
 
 export const register = async (req: express.Request, res: express.Response) => {
